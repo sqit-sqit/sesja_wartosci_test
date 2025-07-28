c02f35d9f5610be4b771055d9dc97f0980922c55
Sqit <adam.grono@gmail.com>
2025-07-28 15:19:00 +0200

    drobne zmiany wyglądu plus korekty wyswietlanych treści

--- intro.py
+++ intro.py
@@ -18,15 +18,27 @@ def pokaz_intro():
     które o tym decydują są często poukrywane w naszej podświadomości, 
     ukształtowane przez naszą przeszłość, doświadczenia, i przeżycia.
     Natomiast te  poukrywane dynamiki kształtują coś, 
-    co nazywamy naszym systemem wartości. 
-    A to już możemy ogarnąć na poziomie świadomym.
+    co nazywamy naszym systemem wartości, czyli zestawu wartości, 
+    które są tak ważne w życiu, że określają nasze postępowanie. 
+    A to już możemy rozpoznać na poziomie świadomym.
                 
     
     Zrozumienie swojego systemu wartości to pierwszy krok do zrozumienia siebie.
     Co mną kieruje. Co dla mnie jest ważne w życiu. Jak to wpływa na moje życiowe priorytety.
-    Jeśli to dla Ciebie ważne, wygospodaruj siebie do pół godziny czasu, tylko dla siebie.  
-    Kliknij przycisk **Zaczynamy** i wejdź w proces refleksji nad własnymi wartościami, 
-    przez który ta aplikacja Ciebie poprowadzi.
+                
+    Jeśli to dla Ciebie ważne, wygospodaruj siebie do pół godziny czasu, tylko dla siebie
+    i poeksploruj, co Ciebie napędza. 
+                
+
+    **UWAGA:** 
+                
+    Dane z tej sesji są logowane. Zostaną zapisane wartości, które wybierzesz, 
+    treść refleksji z formie dialogu z Agentem oraz treść podsumowania. Nie będą logowane  
+    Kliknij przycisk **Zaczynamy** i wejdź w proces refleksji nad własnymi wartościami,
+    przez który ta aplikacja Ciebie poprowadzi. Wszystko po to, by Agenta można było udoskonalać,
+    by lepiej służył kolejnym użytkownikom.
+    Nie będą logowane żadne dane, które umożliwiłyby zidentifikowanie Ciebie.
+    Natomiast jeśli nie wyrażasz zgody na logowanie Twojej sesji, po prostu przerwij proces.
 
 
     # """, unsafe_allow_html=True)
